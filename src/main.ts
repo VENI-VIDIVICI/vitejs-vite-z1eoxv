814f8e5c373fd7f5533eb7c23765b3d54eff50f7
VENI-VIDIVICI <1311735850@qq.com>
2022-08-22 16:21:22 +0800

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import '@surely-vue/table/dist/index.css';
+import 'ant-design-vue/dist/antd.css';
 import STable from '@surely-vue/table';
-createApp(App).use(STable).mount('#app');
+import antV from 'ant-design-vue';
+createApp(App).use(STable).use(antV).mount('#app');
